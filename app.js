13ca82e910a28c9813ea6a61a38c702220232d8f
Shahjahan M.Aslam <shahjahanmuhammadaslam@gmail.com>
2022-11-14 12:55:49 +0500

    assignment 4

---
+++ app.js
@@ -0,0 +1,219 @@
+// chapter 01
+
+alert("Error! Please enter a valid password")
+
+
+var lake ="welcome to js land..."
+var make ="happy coding!"
+var message =lake+"\n"+make
+
+    alert(message)
+
+alert("welcome to js land...")
+
+
+var set ="happy coding!"
+var net ="Prevent this page from creating additional dialogs"
+var message =set+"\n"+net
+
+    alert(message)
+
+alert("Hello... I can run JS through my web broweser's console")
+console.log("Hello... I can run JS through my web broweser's console")
+
+// chapter 1 end
+
+
+// chapter 02
+
+var username = "Aisha";
+console.log(username)
+
+
+alert("hello world")
+alert("Aisha")
+alert("I'm 21 years old")
+alert("Certified mobile application development")
+
+var alpha1 ="PIZZA"
+var alpha2 ="PIZZ"
+var alpha3 ="PIZ"
+var alpha4 ="PI"
+var alpha5 ="P"
+
+var message =alpha1+"\n"+alpha2+"\n"+alpha3+"\n"+alpha4+"\n"+alpha5
+alert(message)
+
+
+alert("my email address is aishaghani2002@gmail.com")
+alert("I am trying to learn from the book A smarter way to learn javcascript")
+alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+document.write("Yah! I can write html content through javascript")
+
+// chapter 02 end
+
+
+
+// chapter 03
+
+var age = ("I'm 21 years old")
+alert(age)
+
+alert("You have visited this site 14 times")
+
+document.write("My birth year is 2002 <br/>")
+
+document.write("data type of my declared variable is number")
+
+document.write("<br><b>John Doe</b>Oredered <b>5 T-shirts</b>(s)on XYZ clothing store")
+
+// chapter 03 end
+
+
+
+// chapter 04
+
+var car = "hello!"
+var cat = "Aisha here"
+var foot = "and I'm 21 years old"
+
+alert(car + cat + foot)
+
+// legal                  
+// var abc                
+// var abc123             
+// var _123               
+// var _abc               
+
+// illegal
+// var @123
+// var *67
+// var alert
+// var javascript
+
+
+document.write("<br><h2>Rules for naming JS variables</h2>")
+
+document.write("<br>Variable must begin with a letter $ or _. for example @name _1st variablbe")
+
+document.write("<br>variable names are case sensitive")
+
+document.write("<br>variable names should not be JS keywords")
+
+
+// chapter 04 end
+
+
+// chapter 05
+
+var one = 5 + 3
+console.log(one)
+
+var two = 5 - 3
+console.log(two)
+
+var so= 5 * 3
+console.log(so)
+
+var ball = 5 / 3
+console.log(ball)
+
+var ant = 4
+ant++
+console.log(ant)
+
+var boat = 5
+++boat
+console.log(boat)
+
+var cup = 7 + 6
+console.log(cup)
+
+var duck = 13
+duck--
+console.log(duck)
+
+var eat = 18 / 6
+console.log(eat)
+
+var fat = 2 - 2
+console.log(fat)
+
+var gun = 600 * 5
+console.log(gun)
+
+
+for (var i = 0; i < 10; i++){
+    console.log("7 X" + (i + 1) + "=" + 7* (i + 1))
+}
+
+
+var abcd = 650 * 3 + 100 * 7 + 100
+console.log(abcd)
+
+var forr = 650 / 800 * 100
+console.log(forr + "%")
+
+var fin = 10 * 104.80 + 25 * 28
+console.log(fin)
+
+var sup = (4 + 1) * 10 / 2
+console.log(sup)
+
+var thin = 2022 - 2004
+console.log(thin)
+
+var cap = (65 - 15) * 3
+console.log(cap)
+
+// chapter 5 end
+
+
+// chapter 6 to 9
+
+var full = 10
+full++
+console.log(full)
+
+var bun = 11
+++bun 
+console.log(bun)
+
+var sole = 12
+sole--
+console.log(sole)
+
+var fall = 11
+--fall
+console.log(fall)
+
+var all = 2
+var but = 1 
+var cow = --all - --but + ++but + but--
+console.log(cow)
+
+var dull = prompt("welcome", "Aisha")
+alert(dull)     
+
+var num = prompt("Enter Table number")
+for (var table = 0; table < 10; table++){
+    console.log(num+"X" + (table + 1) + " = " + num * (table + 1))
+}
+
+
+var sub1 = "english"
+var sub2 = "math"
+var sub3 = "urdu"
+
+var sub1 = 100
+var sub2 = 100
+var sub3 = 100
+
+var sub1 = 80
+var sub2 = 75
+var sub3 = 90
+
+
+var total = 80 / 100 * 100
+
+// chapter 6 to 9 end
